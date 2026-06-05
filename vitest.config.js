1f613099a32ba6694cc5aeda2f4745ece0c4571a
Jiwon Gim <jiwongim@ucar.edu>
2026-06-05 14:42:43 -0600

    add fix ci actions

--- vitest.config.js
+++ vitest.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './vitest.setup.js',
     globals: true,
     include: ['__tests__/**/*.test.{js,jsx}'],
-    alias: {
-      '\\.(css|less|scss|sass)$': './__mocks__/styleMock.js',
-    },
   },
 });
